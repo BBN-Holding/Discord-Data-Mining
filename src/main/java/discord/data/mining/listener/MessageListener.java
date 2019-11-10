45eb227d223f189fb7f7e96861c25726a4bf3c2d
GregTCLTK <gregtcltk@gmail.com>
2019-11-10 14:26:59 +0100

    Command Executed DM

--- src/main/java/discord/data/mining/listener/MessageListener.java
+++ src/main/java/discord/data/mining/listener/MessageListener.java
@@ -3,6 +3,7 @@ package discord.data.mining.listener;
 import discord.data.mining.DataMining;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.PrivateChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -15,31 +16,45 @@ public class MessageListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         if (event.getMessage().getContentRaw().equals("dm!shutdown") || event.getMessage().getContentRaw().equals("dm!stop")) {
-            if (event.getAuthor().getId().equals("477141528981012511") || event.getAuthor().getId().equals("261083609148948488"))
-            try {
-                for (JDA bots : DataMining.bots) {
-                    bots.shutdown();
-                }
+            if (event.getAuthor().getId().equals("477141528981012511") || event.getAuthor().getId().equals("261083609148948488")) {
+                try {
+                    for (JDA bots : DataMining.bots) {
+                        bots.shutdown();
+                    }
 
-                for (JDA clients : DataMining.clients) {
-                    clients.shutdown();
+                    for (JDA clients : DataMining.clients) {
+                        clients.shutdown();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    event.getTextChannel().sendMessage(new EmbedBuilder()
+                            .setTitle("⚠ Error ⚠")
+                            .setDescription("⚠ Error ⚠")
+                            .setColor(Color.RED)
+                            .setTimestamp(Instant.now())
+                            .build()).queue();
+                    event.getTextChannel().sendMessage("```java " + e.toString() + "```").queue();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
                 event.getTextChannel().sendMessage(new EmbedBuilder()
-                        .setTitle("⚠ Error ⚠")
-                        .setDescription("⚠ Error ⚠")
-                        .setColor(Color.RED)
+                        .setTitle("✅ Success ✅")
+                        .setDescription("✅ Success ✅")
+                        .setColor(Color.GREEN)
                         .setTimestamp(Instant.now())
                         .build()).queue();
-                event.getTextChannel().sendMessage("```java " + e.toString() + "```").queue();
+            } else {
+                PrivateChannel Skidder = event.getJDA().getUserById("477141528981012511").openPrivateChannel().complete();
+                PrivateChannel Hax = event.getJDA().getUserById("261083609148948488").openPrivateChannel().complete();
+
+                EmbedBuilder message = new EmbedBuilder()
+                        .setTitle("Command executed by " + event.getAuthor().getAsTag())
+                        .setAuthor(event.getAuthor().getName(), event.getAuthor().getAvatarUrl(), event.getAuthor().getAvatarUrl())
+                        .setDescription(event.getMessage().getContentRaw())
+                        .setTimestamp(Instant.now());
+
+                Skidder.sendMessage(message.build()).queue();
+                Hax.sendMessage(message.build()).queue();
             }
-            event.getTextChannel().sendMessage(new EmbedBuilder()
-                    .setTitle("✅ Success ✅")
-                    .setDescription("✅ Success ✅")
-                    .setColor(Color.GREEN)
-                    .setTimestamp(Instant.now())
-                    .build()).queue();
         }
     }
 }
+
