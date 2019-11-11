1a4171b9784f891cb6d98daef7d4461dfb39b562
GregTCLTK <gregtcltk@gmail.com>
2019-11-11 08:19:39 +0100

    Fixing

--- src/main/java/discord/data/mining/core/DataLog.java
+++ src/main/java/discord/data/mining/core/DataLog.java
@@ -4,15 +4,17 @@ import discord.data.mining.DataMining;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.Event;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 
 import java.awt.*;
 import java.time.Instant;
 
 public class DataLog {
 
-    public static void log(Event event) {
+    public static void log(GenericEvent event) {
         JDA BOT = DataMining.bots.get(DataMining.bot);
         if (event instanceof MessageReceivedEvent) {
             MessageReceivedEvent msgevent = (MessageReceivedEvent) event;
@@ -29,14 +31,17 @@ public class DataLog {
         } else if (event instanceof MessageDeleteEvent) {
             MessageDeleteEvent msgevent = (MessageDeleteEvent) event;
             BOT.getTextChannelById(DataMining.MessageLog).sendMessage(new EmbedBuilder()
-                .setColor(Color.RED)
-                .setTimestamp(Instant.now())
-                .setThumbnail(msgevent.getGuild().getIconUrl())
-                .setTitle("Message deleted")
-                .addField("Guild Name", msgevent.getGuild().getName(), true)
-                .addField("Guild ID", msgevent.getGuild().getId(), true)
-                .addField("Guild Owner", msgevent.getGuild().getOwner().getUser().getAsTag(), true)
-                .build()).queue();
+                    .setColor(Color.RED)
+                    .setTimestamp(Instant.now())
+                    .setThumbnail(msgevent.getGuild().getIconUrl())
+                    .setTitle("Message deleted")
+                    .addField("Guild Name", msgevent.getGuild().getName(), true)
+                    .addField("Guild ID", msgevent.getGuild().getId(), true)
+                    .addField("Guild Owner", msgevent.getGuild().getOwner().getUser().getAsTag(), true)
+                    .build()).queue();
+        } else if (event instanceof MessageReactionAddEvent) {
+            MessageReactionAddEvent revent = (MessageReactionAddEvent) event;
+            // INSERT CODE HERE
         }
     }
 }
--- src/main/java/discord/data/mining/listener/BotMessageListener.java
+++ src/main/java/discord/data/mining/listener/BotMessageListener.java
@@ -1,7 +1,7 @@
 package discord.data.mining.listener;
 
 import discord.data.mining.core.DataLog;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
@@ -9,9 +9,7 @@ import javax.annotation.Nonnull;
 public class BotMessageListener extends ListenerAdapter {
 
     @Override
-    public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
-         if (!event.getGuild().getId().equals("448554629282922527")) {
+    public void onGenericEvent(@Nonnull GenericEvent event) {
              DataLog.log(event);
-         }
     }
 }
