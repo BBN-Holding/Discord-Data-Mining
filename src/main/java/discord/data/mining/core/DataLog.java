517475dfb3ed805de46ccfe55e8ac584d4aeef6f
GregTCLTK <gregtcltk@gmail.com>
2019-12-14 20:04:56 +0100

    Reaction Remove

--- src/main/java/discord/data/mining/core/DataLog.java
+++ src/main/java/discord/data/mining/core/DataLog.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.events.user.UserActivityEndEvent;
 import net.dv8tion.jda.api.events.user.UserActivityStartEvent;
 import net.dv8tion.jda.api.events.user.update.*;
@@ -59,6 +60,22 @@ public class DataLog {
                         .setThumbnail(revent.getGuild().getIconUrl())
                         .setAuthor(revent.getMember().getUser().getAsTag(), revent.getMember().getUser().getAvatarUrl(), revent.getMember().getUser().getAvatarUrl())
                         .setTitle("Reaction added")
+                        .addField("Guild Name", revent.getGuild().getName(), true)
+                        .addField("Guild ID", revent.getGuild().getId(), true)
+                        .addField("Guild Owner", revent.getGuild().getOwner().getUser().getAsTag(), true)
+                        .addField("Reaction", revent.getReaction().toString(), false)
+                        .setFooter("BBN Data Mining", "https://bigbotnetwork.com/images/avatar.png")
+                        .build()).queue();
+            }
+        } else if (event instanceof MessageReactionRemoveEvent) {
+            MessageReactionRemoveEvent revent = (MessageReactionRemoveEvent) event;
+            if (!revent.getGuild().getId().equals("448554629282922527")) {
+                BOT.getTextChannelById(DataMining.ReactionLog).sendMessage(new EmbedBuilder()
+                        .setColor(Color.GREEN)
+                        .setTimestamp(Instant.now())
+                        .setThumbnail(revent.getGuild().getIconUrl())
+                        .setAuthor(revent.getMember().getUser().getAsTag(), revent.getMember().getUser().getAvatarUrl(), revent.getMember().getUser().getAvatarUrl())
+                        .setTitle("Reaction removed")
                         .addField("Guild Name", revent.getGuild().getName(), true)
                         .addField("Guild ID", revent.getGuild().getId(), true)
                         .addField("Guild Owner", revent.getGuild().getOwner().getUser().getAsTag(), true)
